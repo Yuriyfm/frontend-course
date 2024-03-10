a617105e821c1e5e85143c594f6b3d75e390ee2b
efimov_ks402 <iurii.efimov@kaspi.kz>
2024-03-10 13:14:10 +0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
+
+
+ReactDOM.createRoot(
+    document.getElementById("root"),
+).render(
+        <BrowserRouter>
+            <ThemeProvider>
+                <App/>
+            </ThemeProvider>
+        </BrowserRouter>,
+    );
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const MainPageAsync = lazy(() => new Promise(resolve =>
+    setTimeout(() => {
+        // @ts-ignore
+        resolve(import('./MainPage'));
+    }, 1000)));
